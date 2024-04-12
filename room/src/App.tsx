55931907ccfca51b62ea4ba87f0c5ba3f7012d1b
Vijay <vbvijay84@gamil.com>
2024-04-12 07:36:43 +0530

    commit

--- room/src/App.tsx
+++ room/src/App.tsx
@@ -9,7 +9,6 @@
 // import Home from './Pages/Home';
 // import Layout from './Pages/layout';
 
-
 // function App() {
 //   const router = createBrowserRouter([
 //     {
@@ -57,51 +56,62 @@
 // }
 // export default App;
 
-
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
-import Signin from './components/auth/sign-in';
-import Signup from './components/auth/sign-up';
-import MeetingPage from './components/meeting';
-import Upcoming from './Pages/upcoming';
-import Previous from './Pages/previous';
-import Recordings from './Pages/Recordings';
-import PersonalRoom from './Pages/personal-room';
-import Home from './Pages/Home';
-import Layout from './Pages/layout';
-import StreamVideoProvider from './providers/StreamClientProvider';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import Signin from "./components/auth/sign-in";
+import Signup from "./components/auth/sign-up";
+import MeetingPage from "./components/meeting";
+import Upcoming from "./Pages/upcoming";
+import Previous from "./Pages/previous";
+import Recordings from "./Pages/Recordings";
+import PersonalRoom from "./Pages/personal-room";
+import Home from "./Pages/Home";
+import Layout from "./Pages/layout";
+import StreamVideoProvider from "./providers/StreamClientProvider";
 
 function App() {
   return (
     // <div>
     //   <StreamVideoProvider>
     //     <Router>
-          
-          
     //       <Routes>
     //         <Layout>
-    //           <Route> 
-    //               <Route index element={<Home />} />
-    //               <Route path="upcoming" element={<Upcoming />} />
-    //               <Route path="previous" element={<Previous />} />
-    //               <Route path="recordings" element={<Recordings />} />
-    //               <Route path="personal-room" element={<PersonalRoom />} />
+    //           <Route>
+    //             <Route index element={<Home />} />
+    //             <Route path="upcoming" element={<Upcoming />} />
+    //             <Route path="previous" element={<Previous />} />
+    //             <Route path="recordings" element={<Recordings />} />
+    //             <Route path="personal-room" element={<PersonalRoom />} />
     //           </Route>
-    //           </Layout>
+    //         </Layout>
     //         <Route path="signin" element={<Signin />} />
     //         <Route path="signup" element={<Signup />} />
     //         <Route path="meeting/:id" element={<MeetingPage />} />
     //       </Routes>
     //     </Router>
-    //  </StreamVideoProvider>
+    //   </StreamVideoProvider>
     // </div>
     <Router>
-    <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path="signin" element={<Signin />} />
-        <Route path="signup" element={<Signup />} />
-        
-    </Routes>
-  </Router>
+      {/* <StreamVideoProvider> */}
+      {/* <Layout> */}
+        <Routes>
+          <Route element={<Layout/>}>
+            <Route index element={<Home />} />
+            <Route path="upcoming" element={<Upcoming />} />
+            <Route path="previous" element={<Previous />} />
+            <Route path="recordings" element={<Recordings />} />
+            <Route path="personal-room" element={<PersonalRoom />} />
+          </Route>
+          <Route path="signin" element={<Signin />} />
+          <Route path="signup" element={<Signup />} />
+        </Routes>
+      {/* </Layout>/ */}
+      {/* </StreamVideoProvider> */}
+    </Router>
   );
 }
 
--- room/src/main.tsx
+++ room/src/main.tsx
@@ -1,27 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ClerkProvider } from '@clerk/clerk-react'
-import StreamVideoProvider from './providers/StreamClientProvider';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ClerkProvider } from "@clerk/clerk-react";
+import StreamVideoProvider from "./providers/StreamClientProvider";
 
- 
-const VITE_CLERK_PUBLISHABLE_KEY="pk_test_cmVsZXZhbnQtYnVnLTE3LmNsZXJrLmFjY291bnRzLmRldiQ"
+const VITE_CLERK_PUBLISHABLE_KEY =
+  "pk_test_cmVsZXZhbnQtYnVnLTE3LmNsZXJrLmFjY291bnRzLmRldiQ";
 
 // Import your publishable key
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 console.log(PUBLISHABLE_KEY, "PUBLISHABLE_KEY");
 
- 
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
+  throw new Error("Missing Publishable Key");
 }
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <StreamVideoProvider> */}
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <StreamVideoProvider>
         <App />
+      </StreamVideoProvider>
     </ClerkProvider>
-    {/* </StreamVideoProvider> */}
   </React.StrictMode>
-)
+);
